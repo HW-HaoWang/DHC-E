e7196e3a43ddddd5a143c063e29da00a0ae08e36
Hao Wang <HW-HaoWang@users.noreply.github.com>
2021-08-16 18:31:17 -0700

    Update DHC_E.m

--- DHC_E.m
+++ DHC_E.m
@@ -3,7 +3,7 @@ function [T] = DHC_E(dataPath, outPath, outName)
 % This function is used to calculate Whole Graph Embedding
 %
 %
-% Syntax: DHC_E(dataPath)
+% Syntax: DHC_E(dataPath, outPath, outName)
 %
 % Inputs:
 %        dataPath:
@@ -14,44 +14,41 @@ function [T] = DHC_E(dataPath, outPath, outName)
 %                 form of an adjacency matrix or a two-column list,
 %                 including start and end nodes.
 %
-% e.g., adjacency matrix, 4-node graph
-%-------------------------------
-%   : 1 2 3 4
-% 1 : 0 1 1 0
-% 2 : 1 0 0 1
-% 3 : 1 0 0 1
-% 4 : 0 1 1 0
+%                 e.g., adjacency matrix, 4-node graph
+%                 -------------------------------
+%                    : 1 2 3 4
+%                  1 : 0 1 1 0
+%                  2 : 1 0 0 1
+%                  3 : 1 0 0 1
+%                  4 : 0 1 1 0
 %
-% two-column list, 4-node graph
-%     1  2
-%     1  3
-%     2  4
-%     3  4   
-%-------------------------------
+%                 two-column list, 4-node graph
+%                    1  2
+%                    1  3
+%                    2  4
+%                    3  4
+%                 -------------------------------
 %
-%     outPath: The directory where the results are stored.
-%     outName: The name of the results' .csv file is stored.
+%        outPath: The directory where the results are stored.
+%        outName: The name of the results' .csv file is stored.
 %
 % Output:
 %          T:
 %              The whole graph embedding information for each graph.
 %
 % Example:
-%         download the DHC-E from github, then addpath DHC-E to your matlab
-%         path.
-%
-%         [path,~,~] = fileparts(which('DHC_E'));
-%         dataPath = fullfile(path,'Binary_BrainSulc_Net');
-%         [T] = DHC_E(dataPath, path, 'BrainEmbedding') 
+%         [T] = DHC_E('~/Downloads/Project1', '~/Downloads', 'Project1')
 %
 %
 % Reference:
-% Wang, H.,# Deng, Y.,# Lü, L., and Chen, G., (2021), Hyperparameter-free and
+% Wang, H., Deng, Y., Lü, L., and Chen, G., (2021), Hyperparameter-free and
 % Explainable Whole Graph Embedding, https://arxiv.org/abs/2108.02113
-% #, co-first author
 %
 % Hao Wang, UESTC, Chengdu, March 3, 2021, h.wang.psyc(at)gmail.com
 %--------------------------------------------------------------------------
+if nargin < 3
+    error('Check your input parameters, at least 3 parameters are needed')
+end
 
 cd (dataPath)
 fileExt = {'.csv','.txt','xlsx','xls'};
@@ -63,13 +60,19 @@ end
 numGraphs = size(allGraph,1);
 
 for ig = 1:numGraphs
-    g = readmatrix(allGraph(ig).name);
+    % g = readmatrix(allGraph(ig).name);
+    
+    fid = fopen(allGraph(ig).name, 'r');
+    Dir_data = textscan(fid, '', 'Delimiter', ',', 'CollectOutput', true);
+    fclose(fid);
+    g = Dir_data{1};
+    
     if issymmetric(g)==1
         G = graph(g);
     elseif size(g,2)==2
         G = graph(g(:,1),g(:,2));
     else
-        error('The inputed file format is not supported')
+        error('The inputed file format is nor supported')
     end
     
     Hn = node_Hindex_centrality(G);
@@ -101,6 +104,13 @@ T2 = array2table(Embeddings);
 T = [T1 T2];
 
 writetable(T,[outName '.csv']);
+
+% fileID = fopen([outName '.csv'], 'w');
+% fmt = ['%s, ', [repmat('%f, ', 1, NumNumericColumns-1), '%f\n']];
+% for ig = 1:numGraphs
+%     fprintf(fileID, fmt, allGraph(ig).name, Embeddings(ig,:));
+% end
+% fclose(fileID);
 
 end
 
@@ -135,7 +145,7 @@ function [Hn] = node_Hindex_centrality(G)
 %                a binary undirected graph object of MATLAB, see
 %                https://www.mathworks.com/help/matlab/ref/graph.html
 % Outputs:
-%        
+%
 %           Hn:
 %                Nodal H-index centrality of each node of G.
 %
@@ -148,10 +158,10 @@ function [Hn] = node_Hindex_centrality(G)
 %--------------------------------------------------------------------------
 
 % Record the number of nodes
-numNode = size(G.Nodes,1); 
+numNode = size(G.Nodes,1);
 
 % Record the degree centrality for each node
-TotalDeg = degree(G); 
+TotalDeg = degree(G);
 
 % Record the first-order neighbors for each node
 Nei = cell(numNode,1);
@@ -160,26 +170,15 @@ for iNode = 1:numNode
 end
 
 % Initialize the first-order h-index
-Hi = zeros(1, numNode);
-
-for iNode = 1:numNode
-    % first-order neighbors for each node
-    index = Nei{iNode,1};
-    % sort neighbour's degree
-    tmp = sort(TotalDeg(index),'descend');    
-    Hi(iNode)= sum(tmp>=(1:length(tmp))');  
-end
-
-% Continue loop
-Hn(1,:) = Hi;
+Hn(1,:) = TotalDeg;
 
 for inter = 2:numNode
     
-    for iNode = 1:numNode 
+    for iNode = 1:numNode
         Hitmp = Hn(inter-1,:);
         index = Nei{iNode,1};
         tmp = sort(Hitmp(index),'descend');
-        Hn(inter,iNode) = sum(tmp>=(1:length(tmp))); 
+        Hn(inter,iNode) = sum(tmp>=(1:length(tmp)));
     end
     
     if isequal(Hn(end,:),Hn(end-1,:)); break; end
@@ -187,7 +186,5 @@ for inter = 2:numNode
 end
 
 Hn(end,:) = [];
-% add degree back as H0
-Hn = [TotalDeg';Hn];
 
 end
